0ee3f5ac4e59b97878b7287d88b0b1ad2234b42f
kato charles <ckcloudia@gmail.com>
2023-03-17 03:08:47 -0700

    print random numbers

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * Main function - assigns a random number to int n everytime
+ * this program runs and prints n
+ * Return: Always a 0 (means success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	return (0);
+}
