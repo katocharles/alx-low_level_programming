eb1b10608a7678ec271edf494b899edbda341199
kato charles <ckcloudia@gmail.com>
2023-03-21 14:05:59 -0700

    print -putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * main - Entry point to the program
+ * Description: prints word _putchar using putchar prototyp
+ * Returns: Returns 0 always 
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
