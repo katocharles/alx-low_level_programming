6f4e7fbbaca4b9481a437693fb11e61aceec7895
kato charles <ckcloudia@gmail.com>
2023-03-21 15:03:31 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * print_alphabe: function to print the alphabet a - z
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 97; ch <= 122; ++ch)
+		_putchar(ch);
+	_putchar('\n');
+}
