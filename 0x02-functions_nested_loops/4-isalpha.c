594eb70d21ab118ed4fa86ee1763fc688d56243b
kato charles <ckcloudia@gmail.com>
2023-03-21 16:00:45 -0700

    check if letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * _isalpha - checks if character is a letter
+ *            both lowercase or uppercase
+ *
+ * @c: function parameter for chracter.
+ *
+ * Return: returns 1 if c is a letter else 0
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 &&
+	    c >= 65 && c >= 90)
+		return (1);
+	return (0);
+}
