ee8d25ef009ae8249021ecc7b3ef6cf55068d931
kato charles <ckcloudia@gmail.com>
2023-03-21 17:38:59 -0700

    add tow integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * add - add two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: returns total for integer sum
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
+
