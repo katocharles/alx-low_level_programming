a88e28c1694eb81b3d4b683fdc8e2c3067a25c1d
kato charles <ckcloudia@gmail.com>
2023-03-24 03:04:36 -0700

    fix linging errors

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,11 +4,11 @@
  * print_numbers - function to print number 0 - 9
  * Return: returns nothing
  */
-void print_numbers()
+void print_numbers(void)
 {
 	int i;
 
-	for( i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar(i + 48);
 	}
