d3793fe14b0832bfb9c4c0242d6942fed39c3b06
kato charles <ckcloudia@gmail.com>
2023-03-30 07:54:54 -0700

    pointer modify int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+/**
+ * reset_to_98 - takes a pointer to an int parameter
+ *               and updates the value it points to
+ *
+ * @n: int parameter
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
