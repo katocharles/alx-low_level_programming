5fb61d6d17e26922e7b778a76160be3c1f74b5a2
kato charles <ckcloudia@gmail.com>
2023-03-31 01:12:51 -0700

    find string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+/**
+ * _strlen - calculates the length of the string
+ *
+ * @s: string parameter input
+ *
+ * Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; *s != '\0'; ++s)
+		++counter;
+
+	return (counter);
+}
+
