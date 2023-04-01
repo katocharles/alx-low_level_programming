3fc72f4e1aa2af8d276e1a6c95abc42a112b398e
kato charles <ckcloudia@gmail.com>
2023-04-01 02:16:57 -0700

    print new line for zero size

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,16 +4,24 @@
  * print_square - print a square using the character #
  *
  * @size: is the size of the square
-*/
+ */
 
 void print_square(int size)
 {
 	int width, height;
 
-	for (width = 1; width <= size; ++width)
+	if (size == 0)
 	{
-		for (height = 1; height <= size; ++height)
-			_putchar('#');
 		_putchar('\n');
 	}
+	else
+	{
+		for (width = 1; width <= size; ++width)
+		{
+			for (height = 1; height <= size; ++height)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
 }
+
