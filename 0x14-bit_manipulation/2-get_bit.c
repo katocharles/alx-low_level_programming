44080b4673870392281bb0f0a378761199758e92
kato charles <ckcloudia@gmail.com>
2023-07-07 02:35:55 -0700

    refactor function

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,20 +3,25 @@
 /**
  * get_bit - returns the value of a bit at a given index
  * @n: number to check bits in
- * @index: index at which to check bit
+ * @index: index of the bit to retrieve (starting from 0)
  *
- * Return: value of the bit, or -1 if there is an error
+ * Return: value of the bit at index, or -1 if an error occurred
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int bitMask, bitValue;
 
-	if (index > (sizeof(unsigned long int) * 8 - 1))
-		return -1;
+	/* Check if the index is within the valid range */
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
 
-	bitMask = 1 << index;
+	/* Create a bitmask with a 1 at the specified index */
+	bitMask = 1UL << index;
+
+	/* Use bitwise AND to extract the bit value at the specified index */
 	bitValue = n & bitMask;
 
-	return bitValue == bitMask;
+	/* Return 1 if the bit value is non-zero, 0 otherwise */
+	return ((bitValue != 0) ? 1 : 0);
 }
 
